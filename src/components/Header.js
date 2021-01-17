495308f40d2cf269214579fee9a9c5562581752e
Meyer, Andrew <andrewmeyer@granular.ag>
2021-01-17 11:32:53 -0600

    Style updates 2021.

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ class Header extends React.Component {
     render() {
         return (
             <header className="AppHeader">
-                <h1>Hackernews <span role="img" aria-label="Christmas tree">🎄</span></h1>
-                <p>Just a react-based HackerNews viewer.</p>
+                <h1>Hackernews</h1>
+                <p>Just a lil HackerNews viewer.</p>
             </header>
         );
     }
